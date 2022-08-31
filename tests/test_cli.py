6118356aa333c68140323efde4df3a3d9e461edf
deblonde.antoine <antoine.deblonde@protonmail.ch>
2022-08-31 16:09:45 -0400

    formatting tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -11,7 +11,6 @@ from tests.expected_outputs import expected_outputs
 TEST_DATA_FOLDER = pkg_resources.resource_filename('cloudtiger', '../tests')
 TEST_FOLDER = pkg_resources.resource_filename('cloudtiger', '../tests/run')
 
-
 root_folders = [
     ("simple", "."),
     ("absolute", "/app/absolute_root_folder"),
@@ -41,7 +40,6 @@ def test_cli_version(cli_runner, cli_version_flag):
     result = cli_runner(cli_version_flag)
     assert result.exit_code == 0
     assert ', version' in result.output
-    # assert result.output.startswith('cloudtiger, version')
 
 def create_gitops_folder(root_folder):
     """Fixture that create gitops folders using several pathes"""
@@ -62,39 +60,6 @@ def delete_gitops_folder(root_folder):
     if os.path.isdir(root_folder):
         shutil.rmtree(root_folder)
 
-# def test_cli_helper(cli_runner, cli_main_helper):
-#     """Check main helper"""
-#     result = cli_runner(cli_main_helper)
-#     assert result.exit_code == 0
-#     print(result.output)
-#     assert result.output == helper.helpers["basic"]
-
-# def run_test_command(root_folder, scope, command, scenario_name):
-#     """Fixture that runs a command on a scope in a root folder"""
-
-#     # if root_folder[0] == os.path.sep :
-#     #     root_folder = os.path.join(TEST_FOLDER, root_folder[1:])
-#     # else :
-#     #     root_folder = os.path.join(os.getcwd(), root_folder)
-
-#     # we use 'WHITESPACE' as a placeholder for whitespaces in the project root
-#     # because we will split the command input afterwards using whitespaces
-#     # too
-#     root_folder = root_folder.replace(' ', "WHITESPACE")
-
-#     # the command must be fed as a list of parameters
-#     command = format("--project-root %s --output-file"
-#                 "%s --error-file %s %s" %
-#                 (root_folder, 'cloudtiger_std.log',
-#                 'cloudtiger_stderr.log', command))
-#     command = command.split()
-#     command = [elt.replace("WHITESPACE", " ") for elt in command]
-
-#     result = cli_runner(command)
-#     print(result.output)
-#     assert result.output == expected_outputs[scenario_name]
-
-# @pytest.mark.usefixtures("run_test_command_all_scopes_all_root")
 @pytest.mark.parametrize("scenario_commands,scenario_name", [
     (["init 0"], "init_0"),
     (["init 1"], "init_1"),
@@ -125,10 +90,9 @@ def test_cli_test_scenarii(cli_runner, scenario_commands, scenario_name):
             for command in scenario_commands:
                 # output = run_test_command(root_folder, scope, command, scenario_name)
                 ws_root_folder = root_folder.replace(' ', "WHITESPACE")
-                command = format("--project-root %s --output-file "
-                            "%s --error-file %s %s %s" %
-                            (ws_root_folder, 'cloudtiger_std.log',
-                            'cloudtiger_stderr.log', scope, command))
+                command = (f"--project-root {ws_root_folder} --output-file "
+                           f"cloudtiger_std.log --error-file cloudtiger_stderr.log"
+                           f"{scope} {command}")
                 command = command.split()
                 command = [elt.replace("WHITESPACE", " ") for elt in command]
 
