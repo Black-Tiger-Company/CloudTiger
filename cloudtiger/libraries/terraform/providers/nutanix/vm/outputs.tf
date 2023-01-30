8a34ceff1196543f85ab0632362123330a202cba
antoine.deblonde <antoine.deblonde@blacktiger.tech>
2023-01-30 10:18:09 -0500

    debug output for software IP

--- cloudtiger/libraries/terraform/providers/nutanix/vm/outputs.tf
+++ cloudtiger/libraries/terraform/providers/nutanix/vm/outputs.tf
@@ -5,7 +5,8 @@ locals {
 output "vm_parameters" {
   value = {
     vm_name      = nutanix_virtual_machine.virtual_machine.name
-    private_ip   = (!(local.subnet_has_managed_ips)) ? lookup(var.vm, "private_ip", "unset") : nutanix_virtual_machine.virtual_machine.nic_list[0].ip_endpoint_list[0].ip
+    private_ip   = (!(local.subnet_has_managed_ips)) ? lookup(var.vm, "private_ip", "unset") : local.default_ip
+    # private_ip   = (!(local.subnet_has_managed_ips)) ? lookup(var.vm, "private_ip", "unset") : nutanix_virtual_machine.virtual_machine.nic_list[0].ip_endpoint_list[0].ip
     subnet       = var.vm.subnet_name
     network_name = var.vm.network_name
     user         = var.vm.user
